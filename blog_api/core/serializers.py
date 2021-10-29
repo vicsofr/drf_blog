04093b753d2e4195ae4d86dae7eda13f3a587ead
Lev Sentsov <levsenc@mail.ru>
2021-10-29 19:00:01 +0500

    Added: 1) TagView and TagDetailView, that will show us QuerySet of all posts connected with tags and posts connected with the some concrete tag. 2) TagSerializer

--- blog_api/core/serializers.py
+++ blog_api/core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
+from taggit.models import Tag
 from django.contrib.auth.models import User
 
 
@@ -15,4 +16,15 @@ class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
         lookup_field = 'slug'
         extra_kwargs = {
             'url': {'lookup_field': 'slug'}
+        }
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ("name",)
+        lookup_field = 'name'
+        extra_kwargs = {
+            'url': {'lookup_field': 'name'}
         }
--- blog_api/core/views.py
+++ blog_api/core/views.py
@@ -1,10 +1,37 @@
-from rest_framework import viewsets
-from .serializers import PostSerializer
+from rest_framework import viewsets, permissions, pagination, generics
+from .serializers import PostSerializer, TagSerializer
 from .models import Post
 from rest_framework.response import Response
+from taggit.models import Tag
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+
+class TagView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
